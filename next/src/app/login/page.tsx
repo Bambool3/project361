9ccddab532a1f47b9858f0e7b724a9729fd1fb72
Bambool3 <anak_ponggun@cmu.ac.th>
2025-07-24 14:32:19 +0700

    Add loginPage frontend

---
+++ next/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import Login from '../../client/components/login';
+
+const LoginPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
---
+++ next/src/app/page.tsx
@@ -0,0 +1,9 @@
+// MyPage.tsx
+import LoginPage from './login/page';
+import React from 'react';
+
+const MyPage = () => {
+  return <LoginPage />;
+};
+
+export default MyPage;
