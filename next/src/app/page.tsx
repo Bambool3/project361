0a2326d659df417a7c2ced176e0dc44bc88f7670
Chanakarn Palipol <chanakarn.palipol.0808@gmail.com>
2025-08-04 00:10:27 +0700

    feat: add metadata for login page

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,4 +1,9 @@
 import LoginDetails from "@/components/pages/login/LoginDetails";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "CMUPA | Chiang Mai University",
+};
 
 export default function Page() {
     return <LoginDetails />;
