8da926ab3490d3105537958e74760b2cea5ad552
Chanakarn Palipol <chanakarn.palipol.0808@gmail.com>
2025-08-04 00:10:46 +0700

    feat: implement dashboard to components

--- next/src/components/pages/dashboard/DashboardDetails.tsx
+++ next/src/components/pages/dashboard/DashboardDetails.tsx
@@ -1,3 +1,38 @@
+"use client";
+import MainContent from "@/client/components/MainContent";
+import Header from "@/components/header";
+import Sidebar from "@/components/navigations/nav-bar";
+import SubNavBar from "@/components/navigations/sub-nav-bar";
+import { useState } from "react";
+
 export default function DashboardDetails() {
-    return <div>DashboardDetails</div>;
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [activeNavItem, setActiveNavItem] = useState("KPIs");
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const closeMobileMenu = () => {
+        setIsMobileMenuOpen(false);
+    };
+
+    const handleNavItemClick = (itemName: string) => {
+        setActiveNavItem(itemName);
+        closeMobileMenu();
+    };
+    return (
+        <div className="min-h-screen bg-gray-100 font-sans antialiased">
+            <div className="flex flex-col h-screen">
+                <Header
+                    onMenuToggle={toggleMobileMenu}
+                    isMobileMenuOpen={isMobileMenuOpen}
+                />
+                <Sidebar isOpen={isMobileMenuOpen} onClose={closeMobileMenu} />
+                <SubNavBar />
+
+                <MainContent />
+            </div>
+        </div>
+    );
 }
