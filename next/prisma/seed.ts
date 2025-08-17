6eeba419c481bc007f1a15eec260752151fb2c5b
Bambool3 <anak_ponggun@cmu.ac.th>
2025-08-17 19:58:39 +0700

    updated: seed.ts(created category and indicators)

--- next/prisma/seed.ts
+++ next/prisma/seed.ts
@@ -44,6 +44,89 @@ async function main() {
         },
     });
 
+    // Create Category(cmupa)
+    const cmupa = await prisma.category.upsert({
+        where: { id: "01" },
+        update: {},
+        create: {
+            id: "01",
+            name: "CMUPA",
+            description: "test test test 123"
+        },
+    });
+
+    // Create Indicator
+    const indicator01 = await prisma.indicator.upsert({
+        where: { id: "IN01" },
+        update: {},
+        create: {
+            id: "IN01",
+            name: "จำนวนต้นแบบนวัตกรรม",
+            unit: "10 เล่ม",
+            target_value: 100,
+            main_indicator_id: "IN01",
+            responsible_user_id: adminUser.id,
+            category_id: cmupa.id,
+
+        },
+    });
+
+    const indicator01_01 = await prisma.indicator.upsert({
+        where: { id: "0101" },
+        update: {},
+        create: {
+            id: "0101",
+            name: "จำนวนผลงานวิจัย CMU-RL 4-7",
+            unit: "10 เล่ม",
+            target_value: 50,
+            main_indicator_id: indicator01.id,
+            responsible_user_id: adminUser.id,
+            category_id: cmupa.id,
+        },
+    });
+
+    const indicator01_02 = await prisma.indicator.upsert({
+        where: { id: "0102" },
+        update: {},
+        create: {
+            id: "0102",
+            name: "จำนวนนวัตกรรมสิ่งแวดล้อม",
+            unit: "10 เล่ม",
+            target_value: 25,
+            main_indicator_id: indicator01.id,
+            responsible_user_id: adminUser.id,
+            category_id: cmupa.id,
+        },
+    });
+
+    const indicator01_03 = await prisma.indicator.upsert({
+        where: { id: "0103" },
+        update: {},
+        create: {
+            id: "0103",
+            name: "จำนวนนวัตกรรมด้านอาหาร",
+            unit: "10 เล่ม",
+            target_value: 25,
+            main_indicator_id: indicator01.id,
+            responsible_user_id: adminUser.id,
+            category_id: cmupa.id,
+        },
+    });
+
+    const indicatorDepartment = await prisma.indicatorDepartment.upsert({
+        where: {
+            indicator_id_department_id: {
+              indicator_id: indicator01.id,
+              department_id: adminDepartment.id,
+            },
+        },
+        update: {},
+        create: {
+            indicator_id: indicator01.id,
+            department_id: adminDepartment.id
+        },
+    });
+    
     console.log("Database Seeded Successfully!");
 }
 
