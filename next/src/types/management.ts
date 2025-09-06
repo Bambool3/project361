a36364feec7576670776e464f0c45abbdcd77a83
phiraphongph <phiraphong_ph@cmu.ac.th>
2025-09-06 16:52:40 +0700

    add management type

---
+++ next/src/types/management.ts
@@ -0,0 +1,9 @@
+export type Indicator = {
+  id: string;
+  name: string;
+  unit: string;
+  target_value: number;
+  main_indicator_id: string;
+  responsible_user_id: string;
+  category_id: string;
+};
