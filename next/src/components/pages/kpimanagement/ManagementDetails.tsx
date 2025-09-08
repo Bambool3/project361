cb37bdf0f48f14f5d8b00f780bcbe89a10939a11
Chanakarn Palipol <chanakarn.palipol.0808@gmail.com>
2025-09-08 15:07:31 +0700

    feat: delete sub nav bar

--- next/src/components/pages/kpimanagement/ManagementDetails.tsx
+++ next/src/components/pages/kpimanagement/ManagementDetails.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Header from "@/components/header";
 import Sidebar from "@/components/navigations/nav-bar";
-import SubNavBar from "@/components/navigations/sub-nav-bar";
 import { useState } from "react";
 import ManagementMain from "./ManagementMain";
 
@@ -21,7 +20,6 @@ export default function KpiTopicDetails() {
             <div className="flex flex-col h-screen">
                 <Header onMenuToggle={toggleMobileMenu} />
                 <Sidebar isOpen={isMobileMenuOpen} onClose={closeMobileMenu} />
-                {/* <SubNavBar /> */}
 
                 <ManagementMain />
             </div>
