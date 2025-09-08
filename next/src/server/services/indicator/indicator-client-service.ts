b4a553d838a50a9303f397916e5e2c56acec6c84
Chanakarn Palipol <chanakarn.palipol.0808@gmail.com>
2025-09-08 15:10:43 +0700

    error: error

--- next/src/server/services/indicator/indicator-client-service.ts
+++
@@ -1,8 +0,0 @@
-import { Indicator } from "@/types/management";
-
-export class IndicatorService {
-  private static baseUrl = "/api/management";
-  static async getIndicator() {
-    const res = await fetch(this.baseUrl);
-  }
-}
