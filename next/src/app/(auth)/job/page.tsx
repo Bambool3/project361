830edc9e2e945b1cb2715bf023a1efbed75cfd60
Chanakarn Palipol <chanakarn.palipol.0808@gmail.com>
2025-09-10 19:26:22 +0700

    feat: add job and role page

---
+++ next/src/app/(auth)/job/page.tsx
@@ -0,0 +1,10 @@
+import JobDetail from "@/components/pages/job/JobDetail";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "หน่วยงาน | Chiang Mai University",
+};
+
+export default function Page() {
+    return <JobDetail />;
+}
---
+++ next/src/components/pages/role/RoleMain.tsx
@@ -0,0 +1,3 @@
+export default function RoleMain() {
+    return <div>RoleMain</div>;
+}
