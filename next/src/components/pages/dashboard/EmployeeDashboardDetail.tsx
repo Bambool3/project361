b0627416fa7bcfa4a6e3a8fd61b92f09eac612bc
Chanakarn Palipol <chanakarn.palipol.0808@gmail.com>
2025-09-13 18:19:05 +0700

    feat: add employee dashboard view

---
+++ next/src/components/pages/dashboard/EmployeeDashboardDetail.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Header from "@/components/header";
+import NavBar from "@/components/navigations/nav-bar";
+import { useState } from "react";
+
+export default function EmployeeDashboardDetail() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const closeMobileMenu = () => {
+        setIsMobileMenuOpen(false);
+    };
+
+    return (
+        <div className="min-h-screen bg-gray-100 font-sans antialiased">
+            <div className="flex flex-col h-screen">
+                <div
+                    style={{
+                        position: "sticky",
+                        top: 0,
+                        zIndex: 1000,
+                    }}
+                >
+                    <Header onMenuToggle={toggleMobileMenu} />
+                    <NavBar
+                        isOpen={isMobileMenuOpen}
+                        onClose={closeMobileMenu}
+                    />
+                </div>
+
+                <div className="flex-1 bg-gray-100">
+                    {/* <EmployeeDashboardMain /> */}
+                    "EmployeeDashboardMain"
+                </div>
+            </div>
+        </div>
+    );
+}
